66be989cedc8012bdc2c278d6e1b20a39e333837
Tipouroux <perfectum@outlook.fr>
2023-09-11 19:46:11 -0400

    servie worker suppose to be done

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,16 @@
 const CACHE_NAME = 'static-cache-v5';
 
-//Add list of files to cache here.
+
 
 const FILES_TO_CACHE = [
     'offline.html', //*** cest tu suposé detre vide ? ou de mettre tout les page html TOUT MES FICHIER ETC ETC ?***
-    'index.html'
+    'index.html',
+    'offline.html'
 ];
     
     self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
-    // Precache static resources here.
+
     evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
     console.log('[ServiceWorker] Pre-caching offline page');
@@ -21,7 +22,7 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('activate', (evt) => {
 console.log('[ServiceWorker] Activate');
-//Remove previous cached data from disk.
+
 evt.waitUntil(
 caches.keys().then((keyList) => {
 return Promise.all(keyList.map((key) => {
@@ -36,20 +37,43 @@ return caches.delete(key);
 self.clients.claim();
 });
 
-
-
-
-self.addEventListener('install', (evt) => {
-console.log('[ServiceWorker] Install');
-// Precache static resources here.
-self.skipWaiting();
-});
-self.addEventListener('activate', (evt) => {
-console.log('[ServiceWorker] Activate');
-//Remove previous cached data from disk.
-self.clients.claim();
-});
 self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-});
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+    }
+    evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+    return caches.open(CACHE_NAME)
+    .then((cache) => {
+    return cache.match('/Cochenille/PointNClick/offline.html' );      // change ca par tes propre valeurs   ***  return cache.match('/Cochenille/PointNClick/offline.html' );  *** JE DOIS CREER UN PAGE OFFLINE **** avec mon chemin
+    });
+    })
+    );
+    });
+
+
+
+
+
+
+
+
+
+// self.addEventListener('install', (evt) => {
+// console.log('[ServiceWorker] Install');
+// // Precache static resources here.
+// self.skipWaiting();
+// });
+// self.addEventListener('activate', (evt) => {
+// console.log('[ServiceWorker] Activate');
+// //Remove previous cached data from disk.
+// self.clients.claim();
+// });
+// self.addEventListener('fetch', (evt) => {
+// console.log('[ServiceWorker] Fetch', evt.request.url);
+// //Add fetch event handler here.
+// });
